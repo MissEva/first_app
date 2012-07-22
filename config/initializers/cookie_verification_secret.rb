5c3db2fc6666051d1b7d5b923e12295e706ce0a3
Miss Eva <misseva@ubuntu.(none)>
2012-07-22 16:25:15 -0700

    initial commit - rails tutorial

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+
+# ActionController::Base.cookie_verifier_secret = 'f432ea3c79facd161b285246e8d422a96059f7d6dfb502d62b812cb96d1846b621295ee88c738a764374960bd0f42df38a32c57c2c4ce3479219498534584072';
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '75fdee5494f78ec34cf2c94c8084e7633c9115df49e7965db5a64fcc90bb2dc2af47276ef49666b7c3bedfefa832c62c4346c0da5736978447b81e817ee6b94b'
